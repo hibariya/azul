e377ac33179ec03040dd2a696dfd13927a62ee61
hibariya <celluloid.key@gmail.com>
2010-03-12 01:21:36 +0900

    some command created

--- lib/aozora.rb
+++ lib/aozora.rb
@@ -22,22 +22,7 @@ module Aozora
   #$:.unshift(CONF_DIR)
 
   module Reader
-    @commands = []
-
     class << self
-      def run
-        idx = Shelf.new
-        idx.load
-        puts idx.works.inspect
-      end
-
-      def init
-        @commands << :hoge
-      end
-
-      def test
-        puts @commands.inspect
-      end
     end
   end
 
@@ -63,15 +48,13 @@ module Aozora
     class << self
 
       def ready
+        Readline.vi_editing_mode
         Readline.completion_proc = proc {|word|
           Aozora::Command::COMMANDS.grep(/\A#{Regexp.quote word}/)
         }
         command = Aozora::Command.instance
         begin
-          while buf = Readline.readline('> ')
-            #Thread.fork do
-            #puts command.exec(buf)
-            #end
+          while buf = Readline.readline('> ', true)
             command.prepare(buf).call
           end
         rescue SafeExit => f
@@ -82,91 +65,116 @@ module Aozora
     end
   end
 
-  class Session
-    attr_accessor :person, :initial
-
-    def initialize
-      @person = nil
-      @initial = ''
-    end
-
-    __instance = self.new
-    (class << self; self end).
-      __send__(:define_method, :instance) { __instance }
-  end
-
   class SafeExit < RuntimeError; end
 
-  class Command
+  class Command 
     def initialize
       @history = []
+      @session = {}
+      @current = {:width=>40}
     end
 
     def before
       Aozora::Shelf.load if Aozora::Shelf.persons.empty?
+      update(:content=>'', :args=>[], :pipes=>[])
     end
 
-    def prepared(args={})
-      args = {:contents=>'', :overflow=>:none}.merge(args)
-      lambda{system(["echo '#{args[:contents]}' ", args[:overflow]==:auto ? '|less' : ''].join)}
+    def prepared
+      update(:content=>
+             @current[:content].split(/\n/).map{|line|
+               chunked=[];
+               line.scan(/./mu).each_slice(@current[:width].to_i){|l| chunked<<l.join }
+               chunked
+             }.flatten.join("\n"))
+
+        lambda{
+        system(["echo \"#{@current[:content]}\" ",
+               @current[:pipes].empty? ? '' : ['|', @current[:pipes].join('|')].join].join)
+      }
     end
     
-    def prepare(command)
-      @history<<command
-      command, args = command.split(' ')
+    def update(args={})
+      @current.merge!(args)
+      self
+    end
+    
+    def atoz(num)
+      num.to_s(26).scan(/./).map{|c| 'abcdefghijklmnopqrstuvwxyz'.scan(/./)[c.to_i(26)] }.join
+    end
+
+    def prepare(text)
+      commands, *pipes = text.strip.split(/\|/)
+      command, *args = commands.to_s.split(/\s+/)
+      return lambda{} if command.nil?
       begin
-        self.__send__(['__', command.strip, '__'].join, args)
-      rescue NoMethodError
-        prepared(:contents=>"Command [#{command}] not executable.",
-                 :overflow=>:none)
+        before.update({:args=>args, :pipes=>pipes})
+        update(:content=>self.__send__(['__', command.strip, '__'].join))
+        @history<<[command, args, pipes.join('|')].join(' ')
+      rescue NoMethodError => e
+        update(:content=>"Command [#{command}] does not exist.")
+      rescue Exception => e
+        update(:content=>"Command [#{command}] failed(#{e.class}, #{e.message}).")
+      ensure
+        return prepared
       end
     end
 
-    def __index__(args)
-      c = Aozora::Shelf.persons.map{|p| [p.id, p.name].join(' ') }.join("\n")
-      prepared(:contents=>c,
-               :overflow=>:auto)
+    def __index__
+      @session[:persons],c = {},[]
+      Aozora::Shelf.persons.each_with_index{|p,i|
+        @session[:persons][atoz(i)] = p
+         c<<['[', atoz(i), '] ', p.name].join
+      }
+      c.join("\n")
     end
 
-    def __initial__(args)
-      c = Aozora::Shelf.persons.find_all{|p|p.initial==args.strip}.map{|p| [p.id, p.name].join(' ') }.join("\n")
-      prepared(:contents=>c,
-               :overflow=>:auto)
+    def __initial__
+      @session[:persons],c = {},[]
+      Aozora::Shelf.persons.find_all{|p|p.initial==@current[:args].first}.each_with_index{|p,i|
+        @session[:persons][atoz(i)] = p
+         c<<['[', atoz(i), '] ', p.name].join
+      }
+      c.join("\n")
     end
 
-    def __take__(person_id)
-      Aozora::Session.instance.person = Aozora::Shelf.persons.find{|p| p.id==person_id.to_i}
-      c = Aozora::Session.instance.person.works.map{|w| [w.id, w.title].join(' ') }.join("\n")
-      prepared(:contents=>c,
-               :overflow=>:auto)
+    def __take__
+      @session[:works],c = {},[]
+      @session[:persons][@current[:args].first].works.each_with_index{|w,i| 
+        @session[:works][atoz(i)] = w
+        c<<['[', atoz(i), '] ', w.title].join
+      }
+      c.join("\n")
     end
 
-    def __open__(work_id)
-      c = Aozora::Session.instance.person.works.find{|w| w.id==work_id.to_i }.load.source
-      prepared(:contents=>c,
-               :overflow=>:auto)
+    def __open__
+      @session[:works][@current[:args].first].load.source
     end
 
-    def __history__(args)
-      c = @history.join("\n")
-      prepared(:contents=>c,
-               :overflow=>:none)
+    def __history__
+      @history.join("\n")
     end
 
-    def __exit__(work_id)
+    def __exit__
       raise SafeExit, 'Finalizing...'
+    end
+
+    def __set__
+      return if @current[@current[:args].first.intern].nil?
+      @current[@current[:args].first.intern] = @current[:args].last
+      "width => #{@current[:args].last}"
     end
 
     __instance = self.new
     (class << self; self end).
       __send__(:define_method, :instance) { __instance }
 
-    COMMANDS = (self.instance_methods-(Object.instance_methods+%w[prepare prepared before])).
+    COMMANDS = (self.instance_methods-(Object.instance_methods+%w[prepare prepared before update atoz set])).
                 map{|m|m.scan(/[^_]+/)}.flatten
   end
 
   module Shelf
-    BASE_URI = 'http://www.aozora.gr.jp/'
+    #BASE_URI = 'http://www.aozora.gr.jp/'
+    BASE_URI = 'http://mirror.aozora.gr.jp/'
     PERSONS_URI = [BASE_URI, 'index_pages/person_all.html'].join
     PERSON_URI = [BASE_URI, 'index_pages/person%d.html'].join
     WORK_BASE_URI = [BASE_URI, 'cards/%06d'].join
@@ -220,6 +228,7 @@ module Aozora
       
       def load_fetch_uri
         Aozora::Http.fetch(sprintf(WORK_PAGE_URI, @person.id, @id)) do |page|
+        #puts page.read.toutf8.scan(/href=['"]([0-9a-zA-Z_\-\.\/]+\.zip)['"]/mi).inspect
           @fetch_uri = [
             sprintf(WORK_FILES_BASE_URI, @person.id),
             '/',
@@ -263,25 +272,6 @@ module Aozora
     end
   end
 end
-
-Aozora::Shelf.load
-#puts Aozora::Shelf.persons.inspect.gsub(/,/, "\n")
-#puts Aozora::Shelf.works.inspect.gsub(/,/, "\n")
-#Aozora::Shelf.works.first.load
-#Aozora::Shelf.works.map{|wo| puts "person: #{wo.person.name}"; wo.person.works.map{|w| puts "title:#{w.title}" }}
-#puts Aozora::Shelf.works.first.fetch_uri
-#puts Aozora::Shelf.works.first.source
-
-#puts Aozora::Shelf.persons.length
-#Aozora::Shelf.persons.find_all{|p|p.id==879}.each{|p|
-#  puts "#{p.initial_kana}, #{p.initial}, #{p.id}, #{p.name}"
-#  puts '----------------------------'
-#  p.works.each{|w|
-#    puts "#{w.id}, #{w.title}"
-#  }
-#  puts '----------------------------'
-#  puts p.works.find{|w| w.id==14}.load.source
-#}
 
 Aozora::Display.ready
 
