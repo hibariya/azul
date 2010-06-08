e361c3c27152f0d90fec6ba9a4f8c75ce7c0549d
hibariya <hibariya@HIBARIYA.(none)>
2010-06-08 12:35:29 +0900

    いくつか機能を追加

--- lib/aozora/terminal.rb
+++ lib/aozora/terminal.rb
@@ -20,8 +20,22 @@ module Aozora
           shelf = Shelf.open
           work = shelf.works.find{|w|atoz(w.id.to_i)==args.first}
           shelf.fetch work
-        
         end
+
+        def set(args)
+          Aozora::Config.start do
+            __send__(args.first, args.last)
+          end
+          nil
+        end
+
+        def reload(args)
+          Terminal.prepare
+          nil end
+        
+        def updatedb(args)
+          Shelf.open.reload
+          nil end
 
         private
         def atoz(num)
@@ -30,23 +44,31 @@ module Aozora
       end
     end
 
-    def self.ready
+    def self.prepare
       load CONF_FILE if File.exist? CONF_FILE
       Shelf.open.load
-      Readline.vi_editing_mode
-      Readline.completion_proc = lambda {|word|
+      Readline.__send__("#{Aozora.config.editing_mode}_editing_mode") if Aozora.config.editing_mode
+      Readline.completion_proc = lambda do |word|
         (Command.methods-methods).
           grep(/\A#{Regexp.quote word}/)
-      }
+      end
+    end
+
+    def self.ready
+      prepare
       while buf = Readline.readline('> ', true)
+        begin
         command, *pipes = buf.split(/\|/).map!{|m|m.strip}
         command, *args = command.split(/\s/).map!{|m|m.strip}
-        res = Command.__send__(command, args)
-        system("echo \"#{res}\" "+
-               (pipes.empty? ? '': '|'+pipes.join('|')))
+        res = Command.respond_to?(command)? 
+          Command.__send__(command, args): "Command #{command} Not Defined."
+          system("echo \"#{res}\" "+
+                 (pipes.to_s.empty? ? '': '|'+pipes.join('|'))) unless res.to_s.empty?
+        rescue Exception => e
+          puts "#{e.class} #{e.message}"
+        end
       end
 
     end
-
   end
 end
